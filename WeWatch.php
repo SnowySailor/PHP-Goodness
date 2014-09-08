b6fdf384c8a2ddd2664e75b9ff85af3220975ee6
Snow Sailor <kanel.nick@gmail.com>
2014-09-08 17:25:49 -0400

    In theory, this should work.

--- WeWatch.php
+++ WeWatch.php
@@ -2,17 +2,15 @@
 
 $canViewPage = ((Phpfox::getLib('database')->select('user_id')->from(Phpfox::getT('user'))->where('user_id = ' . Phpfox::getUserId())->execute('getSlaveField')) == '' ? 0 : 1);
 if($canViewPage) {
+	echo '<!doctype html><html><head><title>We are watching</title><style>tr, td { border:1px solid black;}</style></head><body><table><tr><td>User Id</td><td>Action</td></tr>';
 	$adminStuff = Phpfox::getLib('database')->select('*')->from('staff_track')->execute('getSlaveRows');
-	$array = array(
-		array('column' => 12, 'column' => 13),
-		array('column' => 15, 'column' => 13)
-	);
+
 	foreach($adminStuff as $key => $value) {
 		$userId = Phpfox::getLib('database')->select('user_name')->from('phpfox_user')->where('user_id = ' . $value['user_id'])->execute('getSlaveField');
 		$action = $value['action'];
 		echo "<tr><td>".$userId."</td><td>".$action."</td></tr>";
 	}
-
+	echo '</table></body></html>';
 }
 
 ?>
