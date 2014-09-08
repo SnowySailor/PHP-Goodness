d229732c841398975a9cc0f38d430b5d23d303b9
Snow Sailor <kanel.nick@gmail.com>
2014-09-08 17:31:09 -0400

    Should only allow PHPFox to use this.

--- WeWatch.php
+++ WeWatch.php
@@ -1,5 +1,7 @@
 <?php
 
+defined('PHPFOX') or exit('NO DICE!');
+
 $canViewPage = ((Phpfox::getLib('database')->select('user_id')->from(Phpfox::getT('user'))->where('user_id = ' . Phpfox::getUserId())->execute('getSlaveField')) == '' ? 0 : 1);
 if($canViewPage) {
 	echo '<!doctype html><html><head><title>We are watching</title><style>tr, td { border:1px solid black;}</style></head><body><table><tr><td>User Id</td><td>Action</td></tr>';
